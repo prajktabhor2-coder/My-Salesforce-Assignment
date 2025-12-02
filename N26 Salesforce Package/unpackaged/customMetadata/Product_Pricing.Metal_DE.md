aa2e3cb9788ca20f24d979e3f34bd2cb36a7848b
prajktabhor2-coder <prajktabhor2@gmail.com>
2025-12-02 12:26:50 +0530

    Add files via upload

---
+++ N26 Salesforce Package/unpackaged/customMetadata/Product_Pricing.Metal_DE.md
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<CustomMetadata xmlns="http://soap.sforce.com/2006/04/metadata" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <label>Metal_DE</label>
+    <protected>false</protected>
+    <values>
+        <field>ATM_Fee__c</field>
+        <value xsi:type="xsd:double">0.0</value>
+    </values>
+    <values>
+        <field>Card_Replacement_Cost__c</field>
+        <value xsi:type="xsd:double">45.0</value>
+    </values>
+    <values>
+        <field>Country_Code__c</field>
+        <value xsi:type="xsd:string">DE</value>
+    </values>
+    <values>
+        <field>Is_Default__c</field>
+        <value xsi:type="xsd:boolean">false</value>
+    </values>
+    <values>
+        <field>Monthly_Cost__c</field>
+        <value xsi:type="xsd:double">1690.0</value>
+    </values>
+    <values>
+        <field>Product_Name__c</field>
+        <value xsi:type="xsd:string">Metal</value>
+    </values>
+</CustomMetadata>
---
+++ N26 Salesforce Package/unpackaged/lwc/productSummary/productSummary.js
@@ -0,0 +1,133 @@
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import getProductSummary from '@salesforce/apex/ProductInfoService.getProductSummary';
+import CASE_CONTACT from '@salesforce/schema/Case.ContactId';
+
+const COLUMNS = [
+  {
+    label: 'Product',
+    fieldName: 'productName',
+    type: 'text'
+  },
+  {
+    label: 'Monthly cost',
+    fieldName: 'monthlyCost',
+    type: 'currency',
+    typeAttributes: { currencyCode: 'EUR', minimumFractionDigits: 2 }
+  },
+  {
+    label: 'ATM fee',
+    fieldName: 'atmFeeLabel',
+    type: 'text'
+  },
+  {
+    label: 'Card replacement',
+    fieldName: 'cardReplacementCost',
+    type: 'currency',
+    typeAttributes: { currencyCode: 'EUR', minimumFractionDigits: 2 }
+  }
+];
+
+export default class ProductSummaryTable extends LightningElement {
+  @api recordId; // Case Id
+
+  columns = COLUMNS;
+  rows;
+  isLoading = true;
+  error;
+
+  contactId;
+
+  // 1) Get Case.ContactId
+  @wire(getRecord, { recordId: '$recordId', fields: [CASE_CONTACT] })
+  wiredCase({ error, data }) {
+    if (data) {
+      this.contactId = data.fields.ContactId
+        ? data.fields.ContactId.value
+        : null;
+      this.error = undefined;
+      this.loadRow(); 
+    } else if (error) {
+      this.contactId = null;
+      this.error = 'Error loading Case.';
+      this.isLoading = false;
+      this.rows = null;
+    }
+  }
+
+  get contactId() {
+    return this.contactId;
+  }
+
+  loadRow() {
+    if (!this.contactId) {
+      this.rows = null;
+      this.isLoading = false;
+      return;
+    }
+
+    this.isLoading = true;
+
+    getProductSummary({ contactId: this.contactId })
+      .then(info => {
+        if (!info) {
+          this.rows = null;
+          this.error = undefined;
+          this.isLoading = false;
+          return;
+        }
+
+        // Build ATM fee label
+        const atmVal = info.atmFee;
+        let atmLabel = '-';
+
+        if (atmVal !== null && atmVal !== undefined) {
+          const n = Number(atmVal);
+          if (!isNaN(n) && Math.abs(n) <= 100) {
+            // treat as percentage
+            atmLabel =
+              (n % 1 === 0
+                ? n.toFixed(0)
+                : n.toFixed(2).replace(/\.?0+$/, '')) + '%';
+          } else {
+            // treat as currency
+            atmLabel = new Intl.NumberFormat('de-DE', {
+              style: 'currency',
+              currency: 'EUR'
+            }).format(n);
+          }
+
+        
+          if (
+            (info.productName === 'Black' || info.productName === 'Metal') && n === 0
+          ) {
+            atmLabel = '0% (Free)';
+          }
+        }
+
+        this.rows = [
+          {
+            productName: info.productName,
+            monthlyCost: info.monthlyCost,
+            atmFee: info.atmFee,
+            atmFeeLabel: atmLabel,
+            cardReplacementCost: info.cardReplacementCost,
+            countryCode: info.countryCode,
+            isDefault: info.isDefault
+          }
+        ];
+
+        this.error = undefined;
+        this.isLoading = false;
+      })
+      .catch(() => {
+        this.error = 'Could not load product information.';
+        this.rows = null;
+        this.isLoading = false;
+      });
+  }
+
+  get noRows() {
+    return !this.rows || this.rows.length === 0;
+  }
+}
